89ad3e637fca46f1a4dfefdbbda0685c987f47e1
SwanHub <jdprince555@gmail.com>
2025-06-30 18:29:59 -0400

    links

--- src/components/shared/Text_ExternalLink.tsx
+++ src/components/shared/Text_ExternalLink.tsx
@@ -13,7 +13,9 @@ export const Text_ExternalLink = ({
     target="_blank"
     className="inline-flex items-center gap-1 text-gray-600 hover:text-violet-700 transition-colors duration-200 no-underline group"
   >
-    <span>{children}</span>
-    <ExternalLink className="w-3 h-3 text-violet-300 group-hover:text-violet-700 transition-colors duration-200" />
+    <span className="underline underline-offset-3 decoration-1 decoration-violet-400">
+      {children}
+    </span>
+    <ExternalLink className="w-3 h-3 text-violet-500 group-hover:text-violet-700 transition-colors duration-200" />
   </Link>
 );
