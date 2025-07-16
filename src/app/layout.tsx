bef559952a1843af250fcd4a65a68816db4fc456
SwanHub <jdprince555@gmail.com>
2025-07-16 02:21:28 -0400

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Section_Header from "@/components/shared/Section_Header";
 import { Section_Footer } from "@/components/shared/Section_Footer";
+import Script from "next/script";
 
 export const inter = Inter({
   subsets: ["latin"],
@@ -20,6 +21,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NVH03VCFTF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-NVH03VCFTF');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.className} antialiased`}>
         <Section_Header />
         {children}
